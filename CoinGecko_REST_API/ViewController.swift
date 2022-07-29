b9ae7261e9928b68c7b1843922f948d286798291
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-29 10:37:20 -0700

    Initial Commit

---
+++ CoinGecko_REST_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinGecko_REST_API
+//
+//  Created by Mitya Kim on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
